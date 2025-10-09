f30a3ee6559d9bcdc302776d127f165635d3bcc9
Muhammad Alif Abrar <136903097+Tesyaf@users.noreply.github.com>
2025-10-09 18:44:29 +0700

    Revert "Controller view"

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +19,3 @@ Route::get('/', function () {
 });
 
 Route::get('/profile/{nama}/{npm}/{kelas}',[ProfileController::class,'profile']);
-
-Route::get('/user', [UserController::class, 'index'])->name('user.index');
-Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('/user', [UserController::class, 'store'])->name('user.store');
